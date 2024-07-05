d2419d41dbb80066d53a93bc359908c7fb0f4ddc
Irbis007 <suskodomk@gmail.com>
2024-07-05 09:23:32 +0300

    changes

--- script.js
+++ script.js
@@ -9,24 +9,63 @@ const nameT = document.getElementById('name')
 
 const form = document.querySelector('.form')
 
-const list = document.querySelector('.list')
+const listBlock = document.querySelector('.list')
 
-console.log(form)
+let arr = []
+let src = ''
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const block = document.createElement('li')
+if(url){
+  url.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+  });
+  
+}
 
-  block.innerHTML = `
-    <h2 class="title">${title.value}</h2>
-    <div class="date">${date.value}</div>
+if(form){
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    arr.push({
+      title: title.value,
+      date: date.value,
+      url: src,
+      text: text.value,
+      nameT: nameT.value,
+      link: link.value
+    })
+  
+    localStorage.setItem('list', JSON.stringify(arr))
+    console.log(39)
+  })
+}
+
+if(listBlock) {
+  const list = JSON.parse(localStorage.getItem('list'))
+  list.forEach(item => {
+    console.log(item)
+    const block = document.createElement('li')
+  
+    block.innerHTML = `
+    <h2 class="title">${item.title}</h2>
+    <div class="date">${item.date}</div>
     <div class="img">
-      <img src="${url.value}"/>
+      <img src="${item.url}"/>
     </div>
-    <p class="text">${text.value}</p>
-    <p class="name">${nameT.value}</p>
-    <a href="${link.value}">Оригинал</a>
-  `
+    <p class="text">${item.text}</p>
+    <p class="name">${item.nameT}</p>
+    <a href="${item.link}">Оригинал</a>
+    `;
+    listBlock.append(block);
+  })
+  console.log(item, 3)
+}
 
-  list.append(block)
-})
+console.log(arr)
+
+console.log(3)
